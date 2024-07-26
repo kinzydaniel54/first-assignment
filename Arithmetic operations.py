ad7c6911fbe693f5d2f78c0ef9c0637110ec143c
kinzydaniel54 <kinzydaniel54@icloud.com>
2024-07-25 23:09:02 -0400

    arithmetic operations in daily life

---
+++ Arithmetic operations.py
@@ -0,0 +1,12 @@
+bread =int("5")
+eggs = int("5")
+milk = int("4")
+
+sum = bread+eggs+milk
+
+print("The sum of all three items is",sum)
+
+SavingsAccount= 700
+interest=("percentage 7%")
+#700 is the int of 7%
+print(SavingsAccount + 700)
